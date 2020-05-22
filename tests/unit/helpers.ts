a7a3093ab5e106956a6e134be851dcea7ca2b54a
eidng8 <cheung.jackey@gmail.com>
2020-05-22 17:16:17 +0800

    update test

--- tests/unit/helpers.ts
+++ tests/unit/helpers.ts
@@ -1,10 +1,10 @@
+/* istanbul ignore file */
+
 /*
  * GPLv3 https://www.gnu.org/licenses/gpl-3.0.en.html
  *
  * Author: eidng8
  */
-
-/* istanbul ignore file */
 
 import { Wrapper } from '@vue/test-utils';
 
